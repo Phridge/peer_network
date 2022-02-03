8adfdbd0782963392647fbd2a90dc2e518db0574
Erdwig <rl.foerster.2002@gmail.com>
2022-02-03 13:56:13 +0100

    Simple Remote Connection test

--- peer_network.py
+++ peer_network.py
@@ -10,8 +10,10 @@ import jsonnode
 
 node = jsonnode.Node(6969)
 
-print(node.ipv4())
-print(node.ipv6_all())
+remote_ip = input("Remote IPv4:")
+
+node.send(jsonnode.Message((remote_ip, 6969), {"Hello": "There!"}))
+print(node.receive())
 
 node.close()
 
